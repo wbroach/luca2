c0a85f7a1bb14996b4906a624807467284970829
Will Broach <william.broach@gmail.com>
2024-04-24 23:24:24 -0400

    add operators to scanner

--- src/main/java/com/luca/Scanner.java
+++ src/main/java/com/luca/Scanner.java
@@ -42,6 +42,15 @@ public class Scanner {
             case '!':
                 addToken(match('=') ? BANG_EQUAL : BANG);
                 break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
 
             default: Luca.error(line, "Unexpected character.");
         }
