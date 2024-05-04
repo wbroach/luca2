03b7ed2dc5c3bc12321ca7021ef09d35816eeb89
Will Broach <william.broach@gmail.com>
2024-05-03 20:55:28 -0400

    parser updates

--- src/main/java/com/luca/Parser.java
+++ src/main/java/com/luca/Parser.java
@@ -17,27 +17,43 @@ public class Parser {
 	}
 
 	private Expr equality() {
-//		Expr expr = comparison();
-//		while (match(BANG_EQUAL, EQUAL_EQUAL)) {
-//			Token operator = previous();
-//			Expr right = comparison();
-//			expr = new Expr.Binary(expr, operator, right);
-//		}
-//
-//		return expr;
-		return binOp(this::comparison, List.of(BANG_EQUAL, EQUAL_EQUAL));
+		return parseBinOp(this::comparison, List.of(BANG_EQUAL, EQUAL_EQUAL));
 	}
 
 	private Expr comparison() {
+		return parseBinOp(this::term, List.of(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL));
 	}
 
-	private Expr binOp(Supplier<Expr> exprSupplier, List<TokenType> opTypes) {
-		Expr expr = exprSupplier.get();
+	private Expr term() {
+		return parseBinOp(this::factor, List.of(MINUS, PLUS));
+	}
+
+	private Expr factor() {
+		return parseBinOp(this::unary, List.of(STAR, SLASH));
+	}
+
+	private Expr unary() {
+		if (match(List.of(BANG, MINUS))) {
+			Token operator = previous();
+			Expr right = primary();
+			return new Expr.Unary(operator, right);
+		}
+
+		return primary();
+	}
+
+	private Expr primary() {
+
+	}
+
+	private Expr parseBinOp(Supplier<Expr> exprType, List<TokenType> opTypes) {
+		Expr expr = exprType.get();
 		while(match(opTypes)) {
 			Token operator = previous();
-			Expr right = exprSupplier.get();
+			Expr right = exprType.get();
 			expr = new Expr.Binary(expr, operator, right);
 		}
+
 		return expr;
 	}
 
