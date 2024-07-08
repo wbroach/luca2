50ec981f480c8ba494cc04ee09b5d1d5d262b576
Will Broach <william.broach@gmail.com>
2024-07-07 22:33:46 -0400

    updates to Resolver.java

--- src/main/java/com/luca/Resolver.java
+++ src/main/java/com/luca/Resolver.java
@@ -2,9 +2,16 @@ package com.luca;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
 @RequiredArgsConstructor
 public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 	private final Interpreter interpreter;
+	private final Stack<HashMap<String, Boolean>> scopes = new Stack<>();
+
 
 	@Override
 	public Void visitAssignExpr(Expr.Assign expr) {
@@ -48,6 +55,9 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
 	@Override
 	public Void visitBlockStmt(Stmt.Block stmt) {
+		beginScope();
+		resolve(stmt.statements);
+		endScope();
 		return null;
 	}
 
@@ -78,11 +88,50 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
 	@Override
 	public Void visitVarStmt(Stmt.Var stmt) {
+		declare(stmt.name);
+		if (stmt.initializer != null) {
+			resolve(stmt.initializer);
+		}
+		define(stmt.name);
 		return null;
 	}
 
 	@Override
 	public Void visitWhileStmt(Stmt.While stmt) {
 		return null;
+	}
+
+	private void resolve(List<Stmt> statements) {
+		for (Stmt statement : statements) {
+			resolve(statement);
+		}
+	}
+
+	private void resolve(Stmt statement) {
+		statement.accept(this);
+	}
+
+	private void resolve(Expr expression) {
+		expression.accept(this);
+	}
+
+	private void beginScope() {
+		scopes.push(new HashMap<String, Boolean>());
+	}
+
+	private void endScope() {
+		scopes.pop();
+	}
+
+	private void declare(Token name) {
+		if (scopes.isEmpty()) { return; }
+
+		Map<String, Boolean> scope = scopes.peek();
+		scope.put(name.lexeme, false);
+	}
+
+	private void define(Token name) {
+		if (scopes.isEmpty()) { return; }
+		scopes.peek().put(name.lexeme, true);
 	}
 }
